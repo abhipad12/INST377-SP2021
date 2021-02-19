95e18535d50fae46cb61e7b6c65b6c62fb33d4fc
abhipad12 <abhishekpad12@gmail.com>
2021-02-19 18:36:04 -0500

    lab4-1

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ app.route('/api')
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
     console.log('Now send something back to your client');
-    res.send('hello world');
+    res.send('hello world'); //changed from console.log('Now send something back to your client')
+ //res.json({data: dataToSendToFrontEnd}); 
   });
 
 app.listen(port, async () => {
@@ -52,6 +53,6 @@ app.listen(port, async () => {
 
 liveReloadServer.server.once('connection', () => {
   setTimeout(() => {
-    liveReloadServer.refresh('/');
+    liveReloadServer.refresh('/'); 
   }, 100);
 });
