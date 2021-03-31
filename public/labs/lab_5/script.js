6af33869976f9285ae22dab6b553844d82bb01fa
abhipad12 <abhishekpad12@gmail.com>
2021-03-31 17:13:08 -0400

    doodle

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,5 +1,11 @@
 function mapInit() {
   var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={pk.eyJ1IjoiYWJoaXBhZCIsImEiOiJja202cWc3YjkwZHk5MnBzOW5oOTM0YTJnIn0.7b8wlS14qH_ERpMAPJB4KA}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
